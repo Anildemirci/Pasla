05b29d7da9011aea7b5b9a6c8dea4f93e2b0fb8a
Anildemirci <anildemirci1996@gmail.com>
2021-12-13 07:19:53 +0300

    Initial Commit

---
+++ Pasla/PaslaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaslaApp.swift
+//  Pasla
+//
+//  Created by Anıl Demirci on 13.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct PaslaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
